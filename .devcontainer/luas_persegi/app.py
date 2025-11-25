f263eea8f2f4564710a78693ff8aa14c20674e38
superi0rf <prismatyass2@gmail.com>
2025-11-25 20:52:17 +0700

    Delete .devcontainer/luas_persegi directory

--- .devcontainer/luas_persegi/app.py
+++
@@ -1,8 +0,0 @@
-# fungsi untuk mencari luas persegi
-def luas_persegi(sisi):
-  luas = sisi*sisi
-  return luas
-
-# Masukan nilai sisi persegi
-s = int(input('Masukkan panjang sisi persegi yang akan dihitung '))
-print("Luas persegi yang Anda inginkan adalah ",luas_persegi(s) )
